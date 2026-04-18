53e8ddada5154189dbd9738efdefcf48f4739f07
Kekadron <zhorik.alabyew@gmail.com>
2026-04-19 01:15:01 +0300

    hotfix: ещё чиним...

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'staticfiles',
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
