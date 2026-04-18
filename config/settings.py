f397ed0666798f66f51ac70fa4af60d6e89fc111
Kekadron <zhorik.alabyew@gmail.com>
2026-04-19 01:25:02 +0300

    hotfix: stable fix

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'config.urls'
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/teacher/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 TEMPLATES = [
